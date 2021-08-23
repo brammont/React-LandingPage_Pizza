3ccb8485fd50b4a1d1747965dc9b4ba3883f3811
brammont <brayanmazuera1@gmail.com>
2021-08-22 21:46:24 -0500

    finish Hero section

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -2,7 +2,10 @@ import React from 'react'
 import {
     HeroContainer,
     HeroContent,
-    
+    HeroItems,
+    HeroH1,
+    HeroP,
+    HeroBtn
   } from './HeroElements';
 import Navbar from '../Navbar';
 
@@ -11,7 +14,11 @@ const Hero = () => {
         <HeroContainer>
             <Navbar/>
             <HeroContent>
-                <h1>fd</h1>
+                <HeroItems>
+                <HeroH1>La mejor Pizza del mundo mundial!</HeroH1>
+                <HeroP>Lista en 60 secondos</HeroP>
+                <HeroBtn>Pedir Orden</HeroBtn>
+            </HeroItems>
             </HeroContent>
         </HeroContainer>
 
