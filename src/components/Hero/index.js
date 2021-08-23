24fac5b1dc2a3ed930313726f5ffc66302183586
brammont <brayanmazuera1@gmail.com>
2021-08-22 21:36:13 -0500

    create HeroContainer with bg

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,11 +1,7 @@
 import React from 'react'
 import {
     HeroContainer,
-    HeroContent,
-    HeroItems,
-    HeroH1,
-    HeroP,
-    HeroBtn
+    
   } from './HeroElements';
 import Navbar from '../Navbar';
 
